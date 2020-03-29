20530281ef657346d3ec65b16c4e041e036971ec
sebastian tobon <setohe.09@gmail.com>
2020-03-28 19:40:42 -0500

    added some polygons to the markup

--- src/pages/index.js
+++ src/pages/index.js
@@ -59,10 +59,12 @@ const DescriptionBanner = styled.p`
   width: 85%;
   text-align: center; 
   margin: -10px auto;
+  font-size: 14px;
 
   ${mq.md(css`
     text-align: left; 
     margin: 0;
+    font-size: 18px;
   `)}
 `
 
@@ -98,6 +100,34 @@ position: relative;
 bottom: 0px;
 height: 2px;`
 
+const BarInterm = styled.div`
+  position: relative;
+  background-image: url(https://modeltheme.com/mt_medplus/wp-content/uploads/2020/03/coronavirus-test-1.jpg);
+  background-position: 53% 1px;
+  background-size: cover;
+  -webkit-clip-path: polygon(100% 0, 100% 84%, 0 100%, 0 23%);
+  clip-path: polygon(100% 0, 100% 84%, 0 100%, 0 23%);
+  height: 470px;
+  margin-top: 20px;
+
+  .overlay {
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    height: 100%;
+    width: 100%;
+    opacity: 0.7;
+    transition: .5s ease;
+    background-color: #2b2d36e0;
+    z-index: -900;
+  }
+
+  ${mq.md(css`
+    background-position: 30% -120px;
+  `)}
+`
 const IndexPage = () => (
   <>
     <SEO title="Home" />
@@ -111,7 +141,7 @@ const IndexPage = () => (
             </Title>
             <DescriptionBanner style={{color: 'white'}}>
               Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s
-              Lorem Ipsum has been the industry's standard dummy text ever since the 1500s Lorem Ipsum has been the industry's standard dummy text ever since the 1500s
+              Lorem Ipsum has been the industry's standard.
             </DescriptionBanner>
           </div>
         </BannerContainer>
@@ -121,17 +151,18 @@ const IndexPage = () => (
               CORONAVIRUS PREVENTION
             </Title>
             <DescriptionBanner>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</DescriptionBanner>
+            <br />
             <Button
               type="common"
               backgroundColor={Colors.mirage}
               backgroundColorHover={Colors.mirage}
             >
-              How To Protect
+              Start The Test
             </Button>
           </div>
         </div>
+        <Hr/>
       </div>
-      <Hr/>
       <Title marginTop="30px" marginBottom="30px" max="10" min="20">
         HOW TO PROTECT YOURSELF
       </Title> 
@@ -140,40 +171,49 @@ const IndexPage = () => (
       <div className="row center-xs center-md">
         <div className="col-xs-6 col-md-4">
           <HowProtect>
-            <img src="https://modeltheme.com/mt_medplus/wp-content/uploads/2020/03/wash.png" />
+            <img src="https://modeltheme.com/mt_medplus/wp-content/uploads/2020/03/wash.png" alt="" />
             <strong>Wash your Hands</strong>
           </HowProtect>
         </div>
         <div className="col-xs-6 col-md-4">
           <HowProtect>
-            <img src="https://modeltheme.com/mt_medplus/wp-content/uploads/2020/03/home-run.png" />
+            <img src="https://modeltheme.com/mt_medplus/wp-content/uploads/2020/03/home-run.png" alt="" />
             <strong>Avoide Close Contact</strong>
           </HowProtect>
         </div>
         <div className="col-xs-6 col-md-4">
           <HowProtect>
-            <img src="https://modeltheme.com/mt_medplus/wp-content/uploads/2020/03/bed.png" />
+            <img src="https://modeltheme.com/mt_medplus/wp-content/uploads/2020/03/bed.png" alt="" />
             <strong>Stay Home</strong>
           </HowProtect>
         </div>
         <div className="col-xs-6 col-md-4">
           <HowProtect>
-            <img src="https://modeltheme.com/mt_medplus/wp-content/uploads/2020/03/cough.png" />
+            <img src="https://modeltheme.com/mt_medplus/wp-content/uploads/2020/03/cough.png" alt="" />
             <strong>Cover Coughs and Sneezes</strong>
           </HowProtect>
         </div>
         <div className="col-xs-6 col-md-4">
           <HowProtect>
-            <img src="https://modeltheme.com/mt_medplus/wp-content/uploads/2020/03/sick.png" />
+            <img src="https://modeltheme.com/mt_medplus/wp-content/uploads/2020/03/sick.png" alt="" />
             <strong>Wear a Face Mask</strong>
           </HowProtect>
         </div>
         <div className="col-xs-6 col-md-4">
           <HowProtect>
-            <img src="https://modeltheme.com/mt_medplus/wp-content/uploads/2020/03/bucket.png" />
+            <img src="https://modeltheme.com/mt_medplus/wp-content/uploads/2020/03/bucket.png" alt="" />
             <strong>Clean and Disinfect</strong>
           </HowProtect>
         </div>
+      </div>
+
+      <div className="row">
+        <BarInterm className="col-xs-12 col-md-12">
+          <Title marginTop="100px" marginBottom="30px" max="10" min="20" color="#fff">
+            WHAT ARE THE SYMPTOMS OF CORONAVIRUS
+          </Title> 
+          <div class="overlay" />
+        </BarInterm>
       </div>
     </div>
 
